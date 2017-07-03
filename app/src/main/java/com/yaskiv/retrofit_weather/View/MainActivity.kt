17afc978f351fac2b963bfa03c5049c6177ac5f1
Yaskiv Anton <yaskiv22@gmail.com>
2017-07-03 16:22:31 +0200

    First screen of weather

--- app/src/main/java/com/yaskiv/retrofit_weather/View/MainActivity.kt
+++ app/src/main/java/com/yaskiv/retrofit_weather/View/MainActivity.kt
@@ -4,6 +4,8 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.EditText
+import android.widget.TextView
 import com.google.gson.ExclusionStrategy
 import com.google.gson.FieldAttributes
 import com.google.gson.GsonBuilder
@@ -16,11 +18,16 @@ import rx.android.schedulers.AndroidSchedulers
 import rx.schedulers.Schedulers
 
 class MainActivity : AppCompatActivity() {
+    var editText: EditText? = null
+    var city_name: TextView? = null
+    var city_temperature: TextView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        editText = findViewById(R.id.Edit_text_city_name) as EditText
+        city_name = findViewById(R.id.City_Name) as TextView
+        city_temperature = findViewById(R.id.City_Temperature) as TextView
     }
 
     fun Click(view: View) {
@@ -43,11 +50,13 @@ class MainActivity : AppCompatActivity() {
         val weatherServiceAPI: WeatherServiceAPI = retrofit.create(
                 WeatherServiceAPI::class.java)
 
-        weatherServiceAPI.getWeather("London", getString(R.string.API_Key))
+        weatherServiceAPI.getWeather(editText!!.text.toString(), getString(R.string.API_Key))
                 .subscribeOn(Schedulers.newThread())
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe(
                         { weather ->
+                            city_name!!.text = weather.name
+                            city_temperature!!.text = (weather.main!!.temp!! - 273.15).toString()
                             Log.d("City : ", weather.name)
                             Log.d("Weather : ", (weather.main!!.temp!! - 273.15).toString())
 
